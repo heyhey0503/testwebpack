dbe20ce889b39a2ce2f92b787490aefc5b50d566
heyhey <20177432+heyhey0503@users.noreply.github.com>
2020-06-14 20:23:07 +0900

    add styled-components

--- src/javascripts/Alert.tsx
+++ src/javascripts/Alert.tsx
@@ -1,10 +1,17 @@
 import * as React from 'react';
+import styled from 'styled-components';
+
+const AlertContainer = styled.div`
+  background-color: #22aacc;
+  color: #fff;
+  padding: 1em;
+`;
 
 const Alert: React.FC<{ message: string }> = ({ message }) => {
   return (
-    <div style={{ backgroundColor: 'green'}}>
+    <AlertContainer>
       {message}
-    </div>
+    </AlertContainer>
   );
 }
 
